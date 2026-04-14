2b65977357423b9e81ce48b7efda312841a1755c
kafunda <emmanuelkafunda01@gmail.com>
2026-04-14 19:13:46 +0200

    Initial secure portfolio setup

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,6 +21,10 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-     path('', include('main.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
